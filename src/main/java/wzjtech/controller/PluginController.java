387825491cd3b7e796fd62410c1b5503b0b90163
Z_J_WANG <z.j.wang@oracle.com>
2021-02-25 16:16:08 +0800

    add some test

--- src/main/java/wzjtech/controller/PluginController.java
+++ src/main/java/wzjtech/controller/PluginController.java
@@ -3,7 +3,13 @@ package wzjtech.controller;
 import com.mongodb.client.result.UpdateResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import wzjtech.document.PluginDocument;
 import wzjtech.service.PluginService;
@@ -20,8 +26,15 @@ public class PluginController {
 
   @PostMapping("/plugin-groups/{groupId}/plugins")
   @ResponseStatus(HttpStatus.CREATED)
-  public Mono<UpdateResult> create(@RequestBody PluginDocument plugin, @PathVariable String groupId) {
+  public Mono<UpdateResult> create(@RequestBody PluginDocument plugin,
+      @PathVariable String groupId) {
     return pluginService.save(groupId, plugin);
+  }
+
+  @PutMapping("/plugin-groups/{groupId}/plugins/{pluginName}")
+  public Mono<UpdateResult> update(@RequestBody PluginDocument plugin, @PathVariable String groupId,
+      @PathVariable String pluginName) {
+    return pluginService.update(groupId, pluginName, plugin);
   }
 
   @GetMapping("/plugin-groups/test")
--- src/main/java/wzjtech/service/PluginService.java
+++ src/main/java/wzjtech/service/PluginService.java
@@ -1,10 +1,11 @@
 package wzjtech.service;
 
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
 import com.mongodb.client.result.UpdateResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -30,8 +31,30 @@ public class PluginService {
    */
   public Mono<UpdateResult> save(String groupId, PluginDocument pluginDocument) {
     var update = new Update().addToSet("plugins", pluginDocument);
-    var criteria = Criteria.where("id").is(groupId);
-    return template.updateFirst(Query.query(criteria), update, PluginGroupDocument.class);
+    var criteria = where("id").is(groupId);
+    return template.updateFirst(query(criteria), update, PluginGroupDocument.class);
+  }
+
+  /**
+   * Update a plugin itself
+   *
+   * @param groupId plugin group id
+   * @param pluginDocument plugin document
+   * @return result
+   */
+  public Mono<UpdateResult> update(String groupId, String pluginName,
+      PluginDocument pluginDocument) {
+    var update = new Update()
+        .set("plugins.$.name", pluginDocument.getName())
+        .set("plugins.$.description", pluginDocument.getDescription())
+        .set("plugins.$.provider", pluginDocument.getProvider())
+        .set("plugins.$.projectUrl", pluginDocument.getProjectUrl());
+
+    //find the group by groupId and update the element in plugins list by plugin name
+    var query = query(where("id").is(groupId))
+        .addCriteria(where("plugins").elemMatch(where("name").is(pluginName)));
+
+    return template.updateFirst(query, update, PluginGroupDocument.class);
   }
 
 }
